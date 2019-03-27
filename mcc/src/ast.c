6984d8eacd5748d14663981f9d7f00b20948ebca
FlorianKrull <floriankrull1507@yahoo.de>
2019-03-27 17:54:29 +0100

    Added mCc_ast_new_literal_bool

--- mcc/src/ast.c
+++ mcc/src/ast.c
@@ -115,6 +115,20 @@ struct mcc_ast_literal *mcc_ast_new_literal_string(char* value)
 	return lit;
 }
 
+struct mcc_ast_literal *mcc_ast_new_literal_bool(bool value)
+{
+	struct mcc_ast_literal *lit = malloc(sizeof(*lit));
+
+	if (!lit) {
+		return NULL;
+	}
+
+	lit -> type = MCC_AST_LITERAL_TYPE_BOOL;
+	lit -> b_value = value;
+
+	return lit;
+}
+
 
 
 void mcc_ast_delete_literal(struct mcc_ast_literal *literal)
